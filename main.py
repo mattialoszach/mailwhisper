c736bda24a2153b78456743b805f0a541daab246
mattialosz <mattia.loszach@gmail.com>
2025-09-12 18:32:52 +0200

    Backend Test (CLI MVP)

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from mailwhisper!")
+from agent.state import initial_state
+from agent.graph import build_graph
+
+def print_state(state):
+    print("\n—— Current Draft ———————————————————")
+    if state.get("to"):      print("To:     ", ", ".join(state["to"]))
+    if state.get("cc"):      print("Cc:     ", ", ".join(state["cc"]))
+    if state.get("subject"): print("Subject:", state["subject"])
+    print("Tone:   ", state.get("tone", "neutral"))
+    print("Body:\n" + (state.get("body", "(empty)") or "(empty)"))
+    print("—————————————————————————————————————\n")
+
+def chat_session():
+    app = build_graph()
+
+    print("\n📧 LangGraph Mail-Drafter (MVP) – structured output\n")
+    
+    # To display intermediate states, we use streaming
+    state = initial_state()
+    for event in app.stream(state, config={"recursion_limit": 300}):
+        #print("---Debug---")
+        #print(event.items())
+        #print("-----------")
+
+        node, patch = next(iter(event.items()))
+        if isinstance(patch, dict):
+            state.update(patch)
+        if node == "apply":
+            print_state(state)
+    
+    if state is not None:
+        print("\n—————————————————————————————————————\n")
+        print("✅ Finished")
+        print_state(state)
+        print("\n—————————————————————————————————————")
 
 
 if __name__ == "__main__":
-    main()
+    chat_session()
